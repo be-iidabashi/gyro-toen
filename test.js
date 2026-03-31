91e1f1035ab942b45582feed49eefcbad35fbd91
be-iidabashi <beengineer.iidabashi@gmail.com>
2026-03-31 18:30:16 +0900

    Web サイト完成

---
+++ test.js
@@ -0,0 +1,11 @@
+const button =document.getElementById("myButton")
+const aaaa = document.getElementById("aaaa")
+aaaa.textContent ="bbbb";
+
+button.addEventListener(
+    "click",
+    function () {
+        console("clicked")
+    }
+)
+
